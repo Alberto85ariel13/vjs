b1226929e9c23ea6ccb878a2cd46a0e28d93a218
mayo84 <43963813+mayo84@users.noreply.github.com>
2019-03-10 15:12:44 -0400

    function name correction in the examples

--- src/object/pick.ts
+++ src/object/pick.ts
@@ -10,8 +10,8 @@ const curryPick = (props: string[]) => (obj: Object) => Object.assign({}, ...pro
  * @param obj
  * @return object
  * @example
- *      v.curryPick(['a','b'],{ a: 3, b: 'v', c: [ 1, 2, 3 ], p: 'v' }); //=> {a:3,b:'v'}
- *      v.curryPick(['a','c'],{ a: 3, b: 'v', c: [ 1, 2, 3 ], p: 'v' }); //=> {a:3,c: [ 1, 2, 3 ]}
+ *      v.pick(['a','b'],{ a: 3, b: 'v', c: [ 1, 2, 3 ], p: 'v' }); //=> {a:3,b:'v'}
+ *      v.pick(['a','c'],{ a: 3, b: 'v', c: [ 1, 2, 3 ], p: 'v' }); //=> {a:3,c: [ 1, 2, 3 ]}
  */
 export function pick(props?: string[], obj?: Object): Object {
     return [pick, curryPick(props), curryPick(props)(obj)][arguments.length];
