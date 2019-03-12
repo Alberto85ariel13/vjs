7c29773583d17109c113c4fa7dff821a8fb7db2a
Alberto Delgado <alberto85ariel13@gmail.com>
2019-03-12 09:51:25 -0400

    added evolve and mergeRigth

---
+++ src/object/evolve.ts
@@ -0,0 +1,18 @@
+const curryEvolve = (applyObj: Object) => (obj: Object) => Object.assign(obj, ...Object.keys(applyObj).map(prop => ({
+    [prop]: applyObj[prop](obj[prop])
+})));
+/**
+ * pick props
+ *
+ * @func
+ * @sig {object} -> {object} -> {object}
+ * @param props
+ * @param obj
+ * @return object
+ * @example
+ *      const mapping = {a:v.add(5), b:v.concat('anilla')}
+ *      v.curryPick(['a','c'],{ a: 3, b: 'v', c: [ 1, 2, 3 ], p: 'v' }); //=> {a:8,b:'vanilla',c: [ 1, 2, 3 ], p: 'v'}
+ */
+export function evolve(applyObj: Object, obj?: Object): Object {
+    return [evolve, curryEvolve(applyObj), curryEvolve(applyObj)(obj)][arguments.length];
+}
